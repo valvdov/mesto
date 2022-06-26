f87290aecb63c4194d6c6ce722d65644080f0c47
Valerii Vdovin <valvdov@gmail.com>
2022-06-26 15:17:45 +0100

    fix JS and CSS after review

--- scripts/index.js
+++ scripts/index.js
@@ -1,41 +1,32 @@
 const popupElement = document.querySelector(".popup");
 const popupCloseButton = popupElement.querySelector(".popup__close-button");
 const popupOpenButton = document.querySelector(".profile__edit-button");
-const popupName = popupElement.querySelector(".popup__name");
-const popupJob = popupElement.querySelector(".popup__job");
-const popupSubmitButton = popupElement.querySelector(".popup__submit-button");
+const popupName = popupElement.querySelector('[name ="popup_name"]');
+const popupJob = popupElement.querySelector('[name ="popup_job"]');
 const profileName = document.querySelector(".profile__name");
 const profileJob = document.querySelector(".profile__job");
-// const likeButton = document.querySelector(".element__like");
+const popupSubmit = popupElement.querySelector(".popup__form");
 
+//Открытие popup
 const openPopup = function (event) {
   popupElement.classList.add("popup_opened");
   popupJob.value = profileJob.textContent;
   popupName.value = profileName.textContent;
 };
 
+//Закрытие popup
 const closePopup = function (event) {
   popupElement.classList.remove("popup_opened");
 };
 
+//отправка popup
 const submitPopup = function (event) {
   profileJob.textContent = popupJob.value;
   profileName.textContent = popupName.value;
-  popupElement.classList.remove("popup_opened");
+  event.preventDefault();
+  closePopup();
 };
-
-const submitPopupByEnter = function (event) {
-  if (event.key === "Enter") {
-    submitPopup();
-  }
-};
-
-// const likeEnable = function (event) {
-//   likeButton.classList.add("element__like_pressed");
-// };
 
 popupOpenButton.addEventListener("click", openPopup);
 popupCloseButton.addEventListener("click", closePopup);
-popupSubmitButton.addEventListener("click", submitPopup);
-popupElement.addEventListener("keypress", submitPopupByEnter);
-// likeButton.addEventListener("click", likeEnable);
+popupSubmit.addEventListener("submit", submitPopup);
